cb4b0be6d025321af247ed96c79621a09ce26009
Henry Borda <henryborda17@gmail.com>
2021-11-29 13:24:05 -0300

    Agrego funcion para obtener secuencias homologas de la secuencia query, ordenados por score maximo con la misma

--- src/project_alignment_optimizer/program/functions.py
+++ src/project_alignment_optimizer/program/functions.py
@@ -6,11 +6,14 @@ import pathlib
 from Bio import SeqIO
 from Bio import AlignIO
 from Bio import Align
+from Bio.Seq import Seq
 from Bio.Align.Applications import ClustalwCommandline
 import re
 import os
 import sys
-import copy
+
+from Bio import Entrez,pairwise2
+from Bio.SeqRecord import SeqRecord
 
 # -------------------
 # Funciones Principales
@@ -54,11 +57,11 @@ def filterAlignment(anAlignment):
     # Saco la secuencia que tiene mas gaps
     # Se puede agregar la comparacion con la query usando pairwise si hay dos con la misma cantidad de gaps
     # (Mas adelante, en esta funcion tambien usamos lo de las secuencias homologas)
-    mostGappedSeq = anAlignment[1]
-    for index in range(2, len(anAlignment)):
-        mostGappedSeq = mostGapped(mostGappedSeq, anAlignment[index],anAlignment[0])
-    printAndLog("Sequence {id} with a lenght of {len} has been removed from the alignment.".format(
-        id=mostGappedSeq.id, len=len(mostGappedSeq.seq.ungap()) ))
+    mostGappedSeq = anAlignment[0]
+    for sequence in anAlignment:
+        mostGappedSeq = mostGapped(mostGappedSeq, sequence,anAlignment[0])
+    printAndLog("Sequence {id} has been removed from the alignment.".format(
+        id=mostGappedSeq.id))
 
     sequences = list(filter(lambda seq: seq.id != mostGappedSeq.id, anAlignment))
     printAndLog(str(len(sequences)) + " sequences left.")
@@ -89,9 +92,8 @@ def gaps(aSequence):
 def getungappedSequences(anAlignment):
     # Obtengo del alineamiento pasado por parametro las secuencias originales
     # Para eso elimino todos los gaps
-    alignment = copy.deepcopy(anAlignment)
     baseSequences = []
-    for sequence in alignment:
+    for sequence in anAlignment:
         sequence.seq = sequence.seq.ungap()
         baseSequences.append(sequence)
     return baseSequences
@@ -123,6 +125,23 @@ def generateTree(alignment):
     tree = alignment
     return tree
 
+def getHomologuesSequencesOrderedByMaxScore(seqQuery):
+    #Se pasa como parametro un SeqRecord
+    #y devuelve una lista de SeqRecord 
+    #ordenada de mayor a menor por score con la seqQuery
+    #Habria que ver si solo traer, por ejemplo, un N definido de seq homologas
+    result = []
+    idsProteins = getIdsHomologuesSequences(seqQuery.id)
+    if idsProteins:
+        idString = ""
+        for idProtein in idsProteins:
+            idString = idString + idProtein + ","
+        Entrez.email = "A.N.Other@example.com"
+        idString = idString[0:len(idString)-1]
+        handle = Entrez.efetch(db="protein", id=idString, rettype="gb", retmode="xml")
+        output = Entrez.read(handle)
+        result = getSequencesOrderedByMaxScore(seqQuery, output)
+    return result
 
 # ---------------------
 # Funciones Auxiliares
@@ -154,7 +173,33 @@ def compareCost(u, v):
             return gap
         return mismatch
 
-
 def printAndLog(msg):
     print(msg)
     log.info(msg)
+
+def getIdsHomologuesSequences(idProtein):    
+    Entrez.email = "A.N.Other@example.com"
+    handle = Entrez.efetch(db="protein", id=idProtein,rettype='ipg', retmode='xml')
+    output = Entrez.read(handle)
+    proteinList = output['IPGReport']['ProteinList']
+    result = []
+    if proteinList:
+        for indx in range(0,len(proteinList)):
+            result.append(proteinList[indx].attributes['accver'])
+
+    return result
+
+def getSequencesOrderedByMaxScore(seqQuery,output):
+    result = []
+    if output:
+        for protein in output:
+            if protein:
+                descriptor = protein["GBSeq_locus"]
+                seqHomologue = SeqRecord(Seq(protein["GBSeq_sequence"].upper()),id =descriptor,name=descriptor,description=descriptor)
+                result.append((seqHomologue,pairwise2.align.globalxx(seqQuery.seq, seqHomologue.seq,score_only=True)))
+        result.sort(key=takeSecond,reverse=True)
+    return [i[0] for i in result] 
+
+def takeSecond(elem):
+    return elem[1]
+
